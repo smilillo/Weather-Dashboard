61298e7d8c06385abafa8a5e4fabe06f1d1ca51b
Sofia <sofiemilillo@gmail.com>
2023-03-07 10:31:49 -0500

    working on functions for current and forecast
    building out cards for forecast in js

--- assets/script.js
+++ assets/script.js
@@ -8,5 +8,22 @@ function getCurrentWeather(data) {
   let currentTemp = weather.main.temp;
   let currentWind - weather.wind.speed;
   let currentHumidity = weather.main.humidity;
-  let iconURL = "https://openweathermap.org/img/wn/10d@2x.png;
+  let iconURL = "https://openweathermap.org/img/wn/10d@2x.png";
+  }
+
+function getForecast() {
+  // Create Cards for 5 Days
+  let cardContainer = document.createElement("div");
+  let cardTitle = document.createElement("h3");
+  cardTitle.textContent = "5-Day Forecast: ";
+  cardContainer.append(cardTitle);
+  
+  let forecastTemp = document.createElement("h4");
+  let forecastWind = document.createElement("h4");
+  let forecastHumidity = document.createElement("h4");
+  let forecastIcon = "https://openweathermap.org/img/wn/10d@2x.png";
+}
+
+// Search History List + Local 
+// 
  
